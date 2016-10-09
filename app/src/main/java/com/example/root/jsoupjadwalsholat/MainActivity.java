2491bbef840e521bc20e82e8d6b117b3d7d54be2
Sulistya <sulistyarif@gmail.com>
2016-10-09 13:45:44 +0700

    sudah membenahi tampilannya, kurang bagian spinner, dan juga memperbaiki title, perlu ditambahkan juga loading nunggu fetch data dari internet

--- app/src/main/java/com/example/root/jsoupjadwalsholat/MainActivity.java
+++ app/src/main/java/com/example/root/jsoupjadwalsholat/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -20,10 +21,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv;
+    TextView tv,tvImsyak,tvShubuh,tvTerbit,tvDhuhur,tvAshar,tvMagrib,tvIsya;
     Button bt;
     List<String> list = new ArrayList<String>();
     Spinner sp;
+    int cityNumb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +35,39 @@ public class MainActivity extends AppCompatActivity {
         tv = (TextView)findViewById(R.id.text);
         bt = (Button)findViewById(R.id.button);
         sp = (Spinner)findViewById(R.id.spinner);
+        tvImsyak = (TextView)findViewById(R.id.textView2);
+        tvShubuh = (TextView)findViewById(R.id.textView4);
+        tvTerbit = (TextView)findViewById(R.id.textView8);
+        tvDhuhur = (TextView)findViewById(R.id.textView6);
+        tvAshar = (TextView)findViewById(R.id.textView10);
+        tvMagrib = (TextView)findViewById(R.id.textView12);
+        tvIsya = (TextView)findViewById(R.id.textView14);
+
 
         ArrayAdapter<String> adapt = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,list);
         adapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp.setAdapter(adapt);
 
+        new scrap().execute();
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                sp.setSelection(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new scrap().execute();
+                cityNumb = sp.getSelectedItemPosition();
+                new scrapTime().execute();
             }
         });
 
@@ -89,6 +114,57 @@ public class MainActivity extends AppCompatActivity {
                 tv.setText(title);
             }
 
+        }
+    }
+
+    public class scrapTime extends AsyncTask<Void,Void,Void>{
+
+        String jamImsyak;
+        String jamShubuh;
+        String jamTerbit;
+        String jamDhuhur;
+        String jamAshar;
+        String jamMagrib;
+        String jamIsya;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try{
+                Document doc = Jsoup.connect("http://jadwalsholatimsak.info/daily.php?id=" + cityNumb).get();
+                Element table = doc.select("table").get(0);
+                Elements rows = table.select("tr");
+
+                Element rowSelected = rows.get(2);
+                Elements colms = rowSelected.select("td");
+
+                jamImsyak = colms.get(1).text();
+                jamShubuh = colms.get(2).text();
+                jamTerbit = colms.get(3).text();
+                jamDhuhur = colms.get(4).text();
+                jamAshar = colms.get(5).text();
+                jamMagrib = colms.get(6).text();
+                jamIsya = colms.get(7).text();
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            tvImsyak.setText(jamImsyak);
+            tvShubuh.setText(jamShubuh);
+            tvTerbit.setText(jamTerbit);
+            tvDhuhur.setText(jamDhuhur);
+            tvAshar.setText(jamAshar);
+            tvMagrib.setText(jamMagrib);
+            tvIsya.setText(jamIsya);
         }
     }
 
